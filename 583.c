f367dc4c63c1c8a1914d7e7d1d942d21428cdf06
Caesar0m0d <Caesar0m0d@gmail.com>
2017-04-04 01:48:36 +0900

    583.c

---
+++ 583.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+	int main(){
+
+int l,m;
+long long n,di,hi,j[100000002];
+
+scanf("%lld %d",&n,&m);
+
+		for(l=0;l<m;l++){
+scanf("%d %d",&di,&hi);
+j[di-1] = hi;
+				}
+
+
+return 0;
+		  }
